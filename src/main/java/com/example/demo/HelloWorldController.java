10aa7267c88c3df6c85fb87f8c67d51056b6a957
Hariharan19986 <hariharan19986@gmail.com>
2022-02-21 15:47:21 +0530

    Four

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,11 @@ public class HelloWorldController {
     @RequestMapping(method = RequestMethod.GET, path = "/hello")
     public String helloWorld(){
         return "Hai Hari";
+    }
+
+    @GetMapping("/hello-bean")
+    public UserDetails helloBean(){
+        return new UserDetails("Hari","A","Karur");
     }
 
 }
