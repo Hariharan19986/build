63d587145917371de45b54931e2c131967114e72
Hariharan19986 <hariharan19986@gmail.com>
2022-02-21 14:04:47 +0530

    Two

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Try Merge
 @SpringBootApplication
 public class DemoApplication {
 
